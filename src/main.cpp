10ebacad8ab2fce96fddd01db534b633172f09d3
Jacob Browning <jacob.browning161@gmail.com>
2024-02-11 01:57:18 -0500

    cleaned up untested code

--- src/main.cpp
+++ src/main.cpp
@@ -18,18 +18,21 @@ Adafruit_BMP280 bmp; // I2C
 // Mic Setup
 static const uint8_t micPin = A2;
 
-
-
-
 #define EASTEREGGS true // Want a fun little suprise?
+#define BMP_DEBUG false
 
 void welcome(void); // welcome message prototype
 
 void setup()
 {
   Serial.begin(9600);
-  while ( !Serial ) delay(100);   // wait for native usb, not sure if I'll keep this in final implementation
-  Serial.println(F("BMP280 test"));
+
+  if (BMP_DEBUG == true)
+  {
+    while (!Serial)
+      delay(100); // wait for native usb, not sure if I'll keep this in final implementation
+    Serial.println(F("BMP280 test"));
+  }
 
   // Screen Begin
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
@@ -40,29 +43,33 @@ void setup()
       ; // Don't proceed, loop forever
   }
 
+  if (BMP_DEBUG == true)
+  {
+    // BMP280 Begin
+    unsigned status;
+    // status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+    status = bmp.begin();
+    if (!status)
+    {
+      Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                       "try a different address!"));
+      Serial.print("SensorID was: 0x");
+      Serial.println(bmp.sensorID(), 16);
+      Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+      Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+      Serial.print("        ID of 0x60 represents a BME 280.\n");
+      Serial.print("        ID of 0x61 represents a BME 680.\n");
+      while (1)
+        delay(10);
+    }
 
-
-  // BMP280 Begin
-  unsigned status;
-  //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
-  status = bmp.begin();
-  if (!status) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                      "try a different address!"));
-    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
-    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
-    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
-    Serial.print("        ID of 0x60 represents a BME 280.\n");
-    Serial.print("        ID of 0x61 represents a BME 680.\n");
-    while (1) delay(10);
+    /* Default settings from datasheet. */
+    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                    Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                    Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                    Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                    Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
   }
-
-  /* Default settings from datasheet. */
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
   welcome();
 }
@@ -74,7 +81,7 @@ void loop()
 void welcome() // Welcome Message
 {
 
-    display.clearDisplay();
+  display.clearDisplay();
 
   display.setTextSize(2); // Draw 2X-scale text
   display.setTextColor(SSD1306_WHITE);
